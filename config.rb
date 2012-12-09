d00c79bb8498f8e4b828a27f46c51718926603bb
Scott Killen <scott@scottkillen.com>
2012-12-09 18:46:06 -0500

    Initial changes

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/ebxl4/"
+http_images_path = "/ebxl4/images"
+http_fonts_path = "/ebxl4/fonts"
+css_dir = "public/ebxl4/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
