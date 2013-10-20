4a4e557e45c98dd2db94008b3ef6ce1d228947e6
JLBShecky <jlbshecky@gmail.com>
2013-10-20 16:41:02 -0400

    Add the project to the  git
    Added the eclipse project to the git.
    
    Did a sample test build with a new build number to make sure the new
    project works correctly.

--- common/ebxl4/lib/ModInfo.java
+++ common/ebxl4/lib/ModInfo.java
@@ -3,7 +3,7 @@ package ebxl4.lib;
 public class ModInfo {
   public static final String MOD_ID       = "EbXL++";
   public static final String MOD_NAME     = MOD_ID;
-  public static final String MOD_VERSION  = "0.0.1.14";
+  public static final String MOD_VERSION  = "0.0.1.15";
   public static final String TESTURE_PATH = MOD_ID.toLowerCase() + ":";
   public static final String CLIENT_PROXY = "ebxl4.proxy.ClientProxy";
   public static final String SERVER_PROXY = "ebxl4.proxy.CommonProxy";
